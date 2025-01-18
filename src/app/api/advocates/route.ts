138b553cec43c79780067a8a8bf83dabdeec4f35
Maria Constanza Mattlin <mcmattlin@Marias-MacBook-Pro.local>
2025-01-17 18:36:52 -0800

    Moved filtering logic to server by querying db directly, installed lodash to use debounce function, cleaned up code

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,27 @@
+import { ilike, or, SQL } from "drizzle-orm";
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  let searchTerm = searchParams?.get("search");
 
-  const data = advocateData;
+  // Set up filters if a search term was passed in
+  let filters: SQL[] = [];
+  if(searchTerm) {
+    searchTerm = `%${searchTerm}%`;
+    filters = [
+      ilike(advocates.firstName, searchTerm),
+      ilike(advocates.lastName, searchTerm),
+      ilike(advocates.city, searchTerm),
+      ilike(advocates.degree, searchTerm),
+      // ilike(advocates.specialties, searchTerm), // TODO handle filtering json
+      // ilike(advocates.yearsOfExperience, searchTerm), // TODO handle filtering int
+    ];
+  }
+
+  const data = await db.select().from(advocates).where(or(...filters));
 
   return Response.json({ data });
 }
